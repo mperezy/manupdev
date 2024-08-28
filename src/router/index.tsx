f9447f2ccc59848477cc01d792eb8ed9e1cdc9e8
Manuel Perez <mperezy@unitru.edu.pe>
2024-08-28 14:18:18 -0500

    Try to fix the support for routes on deployment

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,19 +1,21 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { createHashRouter } from 'react-router-dom';
 import routes from 'router/routes';
 
 import { Home, Portfolio } from 'pages';
 
-export default createBrowserRouter([
+export default createHashRouter([
   {
     path: '*',
     element: <div>Not found page</div>,
   },
   {
     path: routes.home.path,
+    index: true,
     element: <Home />,
   },
   {
     path: routes.portfolio.path,
+    index: true,
     element: <Portfolio />,
   },
 ]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import checker from 'vite-plugin-checker';
 import tsConfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsConfigPaths()],
   base: '/',
+  plugins: [
+    react(),
+    tsConfigPaths(),
+    checker({
+      typescript: true,
+    }),
+  ],
 });
