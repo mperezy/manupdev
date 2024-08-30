d871dd3a827a2ba1553aed3a80b55cc320b3b577
Manuel Perez <mperezy@unitru.edu.pe>
2024-08-29 20:03:45 -0500

    Add design on home page
    - Add react-type-animation package to add typing animation on texts from home page.

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,48 +1,105 @@
-import { useState } from 'react';
-import { Anchor, Button, Flex, Image, Stack, Title } from '@mantine/core';
-import viteLogo from '/vite.svg';
-import reactLogo from 'assets/react.svg';
-import Link from 'components/link';
+import type { ElementType, ReactNode } from 'react';
+import { TypeAnimation } from 'react-type-animation';
+import type { TitleProps } from '@mantine/core';
+import { em, Flex, Image, Stack, Text, Title } from '@mantine/core';
+import { useMediaQuery } from '@mantine/hooks';
 import MainLayout from 'components/main-layout';
-import 'App.css';
+import useTheme from 'hooks/use-theme';
 
-export default () => {
-  const [count, setCount] = useState(0);
+type HeadingProps = {
+  as?: ElementType;
+  color?: TitleProps['c'];
+  children: ReactNode;
+  fontWeight?: TitleProps['fw'];
+};
+
+const Heading = ({ as, color, children, fontWeight }: HeadingProps) => {
+  const isMobile = useMediaQuery(`(max-width: ${em(768)})`);
 
   return (
-    <MainLayout>
-      <Stack align='center'>
-        <Flex gap='md'>
-          <Anchor href='https://vitejs.dev' target='_blank'>
-            <Image h='6rem' src={viteLogo} alt='Vite logo' />
-          </Anchor>
+    <Title
+      order={1}
+      size={isMobile ? '2rem' : '5rem'}
+      c={color}
+      component={as}
+      fw={fontWeight}
+      style={{ textAlign: isMobile ? 'center' : 'left' }}
+    >
+      {children}
+    </Title>
+  );
+};
 
-          <Anchor href='https://react.dev' target='_blank'>
-            <Image
-              h='6rem'
-              src={reactLogo}
-              className='logo react'
-              alt='React logo'
+export default () => {
+  const { isLightTheme } = useTheme();
+  const isMobile = useMediaQuery(`(max-width: ${em(768)})`);
+
+  return (
+    <MainLayout totallyCentered>
+      <Stack w='100%' align='center' py='xl'>
+        <Flex
+          w='100%'
+          direction={{ base: 'column-reverse', md: 'row' }}
+          gap={isMobile ? '2rem' : 'lg'}
+          align='center'
+          justify='space-evenly'
+        >
+          <Stack gap='sm'>
+            <Heading>
+              Hi, I'm{' '}
+              <Heading
+                color={isLightTheme ? '#3FB25F' : '#6dca86'}
+                as='span'
+                fontWeight='900'
+              >
+                Manuel Perez
+              </Heading>
+            </Heading>
+
+            <TypeAnimation
+              key={`type-animation-key-${isMobile}`}
+              sequence={[
+                // Same substring at the start will only be typed out once, initially
+                'a frontend developer',
+                1500, // wait 1s before replacing "Mice" with "Hamsters"
+                'a backend developer',
+                1500,
+                'a sort of Devops engineer',
+                1500,
+                'a code lover',
+                1500,
+              ]}
+              wrapper='span'
+              speed={30}
+              style={{
+                fontSize: '2em',
+                textAlign: 'center',
+                display: isMobile ? 'block' : 'flex',
+              }}
+              repeat={Infinity}
             />
-          </Anchor>
+            <Text
+              w={isMobile ? 'auto' : '95%'}
+              style={{ textAlign: isMobile ? 'center' : 'left' }}
+            >
+              Welcome to my website! you will find some information about me and
+              my professional work. I'm a software engineer who truly loves
+              writing code and diving into new technologies to explore in order
+              to deliver a great product to my consultee.
+            </Text>
+          </Stack>
+
+          <Image
+            src='/images/manu-profile.png'
+            display='flex'
+            w={{ base: '90%', xs: '60%', sm: '20rem', md: '27rem' }}
+            radius='50rem'
+            style={{
+              boxShadow: '2px 2px 57px 7px rgba(20,20,20,0.99)',
+              border: '.25rem #F5F5F5 solid',
+            }}
+          />
         </Flex>
-
-        <Title order={1} size='3rem' style={{ textAlign: 'center' }}>
-          Manu Perez's website will be built here
-        </Title>
-        <Title order={2} size='2.5rem'>
-          Coming soon
-        </Title>
-
-        <Link to='/portfolio'>Go to portfolio page</Link>
-
-        <Button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </Button>
-
-        <p className='read-the-docs'>
-          Click on the Vite and React logos to learn more
-        </p>
       </Stack>
     </MainLayout>
   );
