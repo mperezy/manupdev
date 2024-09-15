ec900f17b4b22e0680768b68c1de25f5078a3344
Manuel Perez <mperezy@unitru.edu.pe>
2024-09-15 00:43:14 -0500

    Add box-shadow to base-modal

--- src/components/base-modal.tsx
+++ src/components/base-modal.tsx
@@ -1,5 +1,6 @@
 import type { ReactNode } from 'react';
 import type { ModalProps } from '@mantine/core';
+import { Paper } from '@mantine/core';
 import { Title } from '@mantine/core';
 import { Modal } from '@mantine/core';
 import type { ModalBaseProps } from 'providers/modal/types';
@@ -12,29 +13,31 @@ type Props = Omit<ModalProps, 'opened' | 'onClose'> &
   };
 
 export default ({ children, title, onCloseCallback, ...rest }: Props) => (
-  <Modal
-    {...rest}
-    onClose={() => {
-      onCloseCallback?.();
-      rest.onClose();
-    }}
-    centered
-    keepMounted={false}
-    title={
-      <Title
-        variant='heading'
-        component='span'
-        fz={{ base: '1.5rem', md: '2.125rem' }}
-        style={{ transition: 'all 0.5s' }}
-      >
-        {title}
-      </Title>
-    }
-    overlayProps={{
-      backgroundOpacity: 0.55,
-      blur: 4,
-    }}
-  >
-    {children}
-  </Modal>
+  <Paper shadow='xl'>
+    <Modal
+      {...rest}
+      onClose={() => {
+        onCloseCallback?.();
+        rest.onClose();
+      }}
+      centered
+      keepMounted={false}
+      title={
+        <Title
+          variant='heading'
+          component='span'
+          fz={{ base: '1.5rem', md: '2.125rem' }}
+          style={{ transition: 'all 0.5s' }}
+        >
+          {title}
+        </Title>
+      }
+      overlayProps={{
+        backgroundOpacity: 0.55,
+        blur: 4,
+      }}
+    >
+      {children}
+    </Modal>
+  </Paper>
 );
